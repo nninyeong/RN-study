2f60bf14bc6e9fdf648ecc04370d62fbb671557f
minyeong <minyk0512@gmail.com>
2025-02-02 16:46:59 +0900

    section2: 이벤트핸들러 추가하기

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import { StyleSheet, Button, TextInput, View, Text } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoalText);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="수강 목표를 입력해보세요!"
+          onChangeText={goalInputHandler}
         />
-        <Button title="추가하기" />
+        <Button title="추가하기" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>목표 리스트</Text>
